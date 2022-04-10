0e5eacf156564d6603140617c084f029036f1ca6
Ben <bdecor227@gmail.com>
2022-04-10 07:25:18 -0400

    importing done. also added licenses

--- matlab/expoupdate.m
+++ matlab/expoupdate.m
@@ -14,29 +14,6 @@ function [mxLkLam,mnPostLam,theta,postgam,postmu] = expoupdate(n,T,theta0,nb,fig
 % with values for lambda in the first column and corresponding p values in
 % the second column; postmu is the same distribution after change of
 % variable (from lambda  to mu)
-
-% MIT License
-% 
-% Copyright (c) 2022 bendecorte
-% 
-% Permission is hereby granted, free of charge, to any person obtaining a copy
-% of this software and associated documentation files (the "Software"), to deal
-% in the Software without restriction, including without limitation the rights
-% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-% copies of the Software, and to permit persons to whom the Software is
-% furnished to do so, subject to the following conditions:
-% 
-% The above copyright notice and this permission notice shall be included in all
-% copies or substantial portions of the Software.
-% 
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-% SOFTWARE.
-
 if nargin<4
     nb = 100;
 end
