d1b33b2ad7afe18b9b846cc923d981fed41863ca
bendecorte <bdecor227@gmail.com>
2022-04-14 15:14:10 -0400

    octave compatability and dual-bernoulli function

--- matlab/UpdatingGamPostAndExpSource.m
+++ matlab/UpdatingGamPostAndExpSource.m
@@ -19,54 +19,50 @@
 %  SOFTWARE.
 
 isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
-if isOctave 
-	pkg load statistics  % for Octave
-end
+if isOctave; pkg load statistics; end
 
+%% fig setup
 set(0,'defaultAxesFontSize',24)
 close
-figure; set(gcf,'OuterPosition',[ 663.00 1299.00 653.00 1091.00])
+figure; set(gcf,'units','normalized','OuterPosition',[0.1961 0.0250 0.2858 0.7380])
 ax(1) = subplot(2,1,1); set(gca,'FontSize',18)
 ax(2) = subplot(2,1,2); set(gca,'FontSize',18)
+
+%% analysis
 t = linspace(0,50); % support for the Source distribuution
 theta0 = [.5 0]; % Jeffreys prior (initial value of gammapdf pameter vector)
 D = exprnd(10,50,1); % 50 draws from an exponential distribution with a mean
 % interval between events of 10 s
-  
+
 for d = 1:50 % stepping through the data
     [~,mnPostLam,theta,postgam] = expoupdate(d,sum(D(1:d)),theta0);
     % mnPostLam = estimate of exponential lambda; theta = updated parameter 
     % vector of gammapdf posterior; postgam = updated posterior
     % gamma distribution--but with unnomrmalized probability density vector
     % hence likelihoods not probability densities
-    if isOctave
-	hold off
-    else
-	hold(ax,'off')
-    end
-    plot(ax(1),postgam(:,1),postgam(:,2),'k-','LineWidth',2) % plotting posterior
-    xlim(ax(1),[0 1])
-    ylim(ax(1),[0 .03])
-    if isOctave
-	hold on
-    else
-    	hold(ax(1),'on') 
-    end
 
+    hold(ax(1),'off'); hold(ax(2),'off');
+    plot(ax(1),postgam(:,1),postgam(:,2),'k-','LineWidth',2) % plotting posterior
+    plot(ax(2),t,exppdf(t,1/mnPostLam),'k-','LineWidth',2) % plotting Source
+    
+    hold(ax(1),'on')
     plot(ax(1),[.1 .1],ylim(ax(1)),'k--','LineWidth',1) % true value of lambda
     d100 = .01*max(postgam(:,2)); % 100 times less likely than the max likelihood
     plot(ax(1),xlim,[d100 d100],'k:','LineWidth',2)
-    plot(ax(2),t,exppdf(t,1/mnPostLam),'k-','LineWidth',2) % plotting Source
+
+    title(ax(1),'Posterior Likelihood Function','FontWeight','normal','FontSize',24)
     ylabel(ax(1),'Likelihood','FontSize',24)
     xlabel(ax(1),'Possible Values for \lambda','FontSize',24)
+    xlim(ax(1),[0 1])
+    ylim(ax(1),[0 .03])
+    title(ax(2),'Estimated Source Distribution','FontWeight','normal','FontSize',24)
     ylabel(ax(2),'Probability Density','FontSize',24)
     xlabel(ax(2),'d = Duration (s)','FontSize',24)
-    title(ax(1),'Posterior Likelihood Function','FontWeight','normal','FontSize',24)
-    title(ax(2),'Estimated Source Distribution','FontWeight','normal','FontSize',24)
-    text(ax(1),.15,.028,...
+
+    text(ax(1),.15,max(ylim(ax(1)))*.85,...
         ['p(\lambda|D) \propto gampdf(\lambda=0:.01:1, n=' num2str(d) ', D=' num2str(sum(D(1:d)),3) ')'],...
         'FontSize',24) % expression for the posterior distribution
-    text(ax(2),10,.09,'p^{\prime} = \lambdae^{-\lambdad}','FontSize',36) %
-    % expression for the exponential distribution
+    text(ax(2),10,max(ylim(ax(2)))*.85,'p^{\prime} = \lambdae^{-\lambdad}','FontSize',36) %
+
     pause
 end
---
+++ matlab/updatingBetaPostAndBernoulliSource.m
@@ -0,0 +1,40 @@
+function updatingBetaPostAndBernoulliSource(p1,p2)
+% press any key to see next update
+close
+set(0,'defaultAxesFontSize',24)
+figure; set(gcf,'units','normalized','OuterPosition',[0.1839 0 0.1814 0.8898])
+ax(1) = subplot(2,1,1);
+ax(2) = subplot(2,1,2);
+D1 = binornd(1,p1,256,1); % 200 draws from a Bernoulli distribution
+% with parameter p1
+D2 = binornd(1,p2,256,1); % ditto but w parameter p2
+theta0 = [.5 .5];
+
+for d = [2 4 8 16 32 64 128 256]
+    [Pest1,theta1,Pdist1,Pest2,theta2,Pdist2] = BayesEstBernP(D1(1:d),theta0,D2(1:d));
+
+    plot(ax(1),Pdist1(:,1),Pdist1(:,2),'k-',Pdist2(:,1),Pdist2(:,2),'b-','LineWidth',2)
+    % the two posteriors on same axes
+    xlabel(ax(1),'\itp');ylabel(ax(1),'Likelihood')
+    title(ax(1),'Beta Posteriors','FontWeight','normal')
+    legend(ax(1),'p1','p2','Location','northwest')
+    Ylm = ylim(ax(1));
+    text(ax(1),.05,.5*Ylm(2),['\itn\rm = ' num2str(d)],'FontSize',24)
+    text(ax(1),.05,.38*Ylm(2),['\theta_1=[' num2str(theta1(1)) ...
+        ' ' num2str(theta1(2)) ']'],'FontSize',24)
+    text(ax(1),.05,.26*Ylm(2),['\theta_2=[' num2str(theta2(1)) ' ' ...
+        num2str(theta2(2)) ']'],'FontSize',24)
+    %hold(ax(1),'off')
+    
+    h(1) = bar(ax(2),[1 2],[1-Pest1 Pest1],.8,'FaceColor',[0 0 0]);
+    hold(ax(2),'on')
+    h(2) = bar(ax(2),[4 5],[1-Pest2 Pest2],.8,'FaceColor','b');
+    set(gca,'xtick',[1 2 4 5],'xticklabel',{'0' '1' '0' '1'})
+    ylabel(ax(2),'Probability')
+    xlabel(ax(2),'Bernoulli Support')
+    title(ax(2),'Bernoulli Source Distributions','FontWeight','normal')
+    hold(ax(2),'off')
+    sprintf('\npress any key to see next update')
+    pause
+end
+
